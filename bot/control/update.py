16fcae5d0f1b1a6c9359fefeb42978299c2aaf73
xiidot1303 <xii1303@inbox.ru>
2023-05-11 08:52:14 +0500

    fix Dispatcher

--- bot/control/update.py
+++ bot/control/update.py
@@ -1,6 +1,6 @@
 from telegram import Bot
 from telegram.ext import Dispatcher, PicklePersistence
-from telegram.ext import Updater
+from telegram.ext import Updater, JobQueue
 from bot.control.handlers import handlers
 from config import BOT_API_TOKEN, DEBUG
 
@@ -11,7 +11,7 @@ bot_obj = Bot(BOT_API_TOKEN)
 
 if not DEBUG:  # in production
     updater = 1213
-    dp = Dispatcher(bot_obj, None, workers=1000, use_context=True, persistence=persistence)
+    dp = Dispatcher(bot_obj, None, workers=1000, use_context=True, persistence=persistence, job_queue=JobQueue)
 
 else:  # in development
     updater = Updater(
