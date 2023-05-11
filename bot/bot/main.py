76c8e441b1139d59289ea72449311d2773928a07
xiidot1303 <xii1303@inbox.ru>
2023-05-11 07:14:23 +0500

    send chat action via messaging

--- bot/bot/main.py
+++ bot/bot/main.py
@@ -2,12 +2,14 @@ from bot.bot import *
 from app.services.api_service import request_chat_gpt
 
 def start(update, context):
+    bot_send_chat_action(update, context)
     update_message_reply_text(
         update, 
         "Hello! I am AI assistant. How can I help you?"
         )
 
 def chat(update, context):
+    bot_send_chat_action(update, context)
     message = update.message.text
     response = request_chat_gpt(message)
     update_message_reply_text(update, response)
